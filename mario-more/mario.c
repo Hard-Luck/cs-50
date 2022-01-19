6d9e9d8bb394610d9f651ace68c36e6df9f006ca
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-01-19 21:05:48 +0000

    Wed, Jan 19, 2022, 9:05 PM +00:00

---
+++ mario-more/mario.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int n = get_int("Height: ");
+    for(int i=n; i > 0; i--)
+        {
+        for(int j=(n-1); j > 0; j--)
+        {
+            printf(" ");
+        for(int k=1; k >= n ; k++)
+        {
+            printf("#");
+        }
+        }
+        printf(" \n");
+        }
+}
