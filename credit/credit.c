248dc889db38f064fa4527293bac6c86cf4664e6
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-01-20 11:18:34 +0000

    Thu, Jan 20, 2022, 11:18 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -3,15 +3,22 @@
 #include <math.h>
 
 int test = 1;
-
+int length(long card);
 int main(void)
 {
     long card = get_long("Enter card number: ");
+    int x = length(card);
+    printf("%d\n",x);
+}
+
+
+int length(long card)
+{
     int count = 0;
     while (card != 0)
     {
         card = card/10;
         count++;
     }
-    printf("%d",count);
+    return count;
 }
