276d3b4ddf9468d91f5f6e258268d8c07838ccd6
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-01-20 13:45:35 +0000

    Thu, Jan 20, 2022, 1:45 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -51,24 +51,24 @@ int length(long card)
 
 string check_type(long card, int length)
 {
-    int first_one = card / pow(10, length-1);
-    int first_two = card / pow(10, length-2);
-    if ((length == 13 || length == 16) && first_one == 4)
-    {
-        return "VISA";
-    }
-    else if ((length == 15) && ((first_two == 34) || (first_two == 37)))
-    {
-        return "AMEX";
-    }
-    else if ((length == 16) && ((first_two > 50) && (first_two <= 55)))
-    {
-        return "MASTERCARD";
-    }
-    else
-    {
-        return "INVALID";
-    }
+int first_one = card / pow(10, length - 1);
+int first_two = card / pow(10, length - 2);
+if ((length == 13 || length == 16) && first_one == 4)
+{
+    return "VISA";
+}
+else if ((length == 15) && ((first_two == 34) || (first_two == 37)))
+{
+    return "AMEX";
+}
+else if ((length == 16) && ((first_two > 50) && (first_two <= 55)))
+{
+    return "MASTERCARD";
+}
+else
+{
+    return "INVALID";
+}
 }
 
 
