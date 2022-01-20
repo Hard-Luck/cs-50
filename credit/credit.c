03a04eb1d6a8daf17143dd5fc3160621cf0e43e2
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-01-20 13:43:52 +0000

    Thu, Jan 20, 2022, 1:43 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -15,7 +15,7 @@ int main(void)
         int checksum = sumdigit(card, x);
         if (checksum % 10 == 0)
         {
-            printf("%s\n",check_type(card,x));
+            printf("%s\n", check_type(card, x));
         }
         else
         {
@@ -43,75 +43,75 @@ int length(long card)
     int count = 0;
     while (card != 0)
     {
-        card = card/10;
+        card = card / 10;
         count++;
     }
     return count;
 }
 
 string check_type(long card, int length)
-    {
-        int first_one = card/pow(10,length-1);
-        int first_two = card/pow(10,length-2);
-        if ((length == 13 || length == 16) && first_one == 4)
-        {
-            return "VISA";
-        }
-        else if ((length == 15) && ((first_two == 34) || (first_two == 37)))
-        {
-            return "AMEX";
-        }
-        else if ((length == 16) && ((first_two > 50) && (first_two <= 55)))
-        {
-            return "MASTERCARD";
-        }
-        else
-        {
-            return "INVALID";
-        }
-    }
-
-
-int sumdigit(long card,int len)
 {
-    int sum = 0;
-    long card_odd = card;
-    long card_ev = card;
-    if (len % 2 == 0)
+    int first_one = card / pow(10, length-1);
+    int first_two = card / pow(10, length-2);
+    if ((length == 13 || length == 16) && first_one == 4)
     {
-        for (int i = 1; i < len; i += 2)
-        {
-            sum += (card_odd % 10);
-            card_odd /= 100;
-        for (int j = 1; j < len; j += 2)
-        {
-            int digits = (2*((card_ev % 100)/10));
-            if (digits >= 10)
-            {
-                digits =((digits % 10) + (digits / 10));
-            }
-            sum += digits;
-            card_ev /= 100;
-        }
-        }
+        return "VISA";
+    }
+    else if ((length == 15) && ((first_two == 34) || (first_two == 37)))
+    {
+        return "AMEX";
+    }
+    else if ((length == 16) && ((first_two > 50) && (first_two <= 55)))
+    {
+        return "MASTERCARD";
     }
     else
     {
-        for (int a = 1; a <= len; a += 2)
-        {
-            sum += (card_odd % 10);
-            card_odd /= 100;
-        }
-        for (int b = 2; b < len; b += 2)
-        {
-            int digits = (2*((card_ev % 100)/10));
-            if (digits >= 10)
-            {
-                digits =((digits % 10) + (digits / 10));
-            }
-            sum += digits;
-            card_ev /= 100;
-        }
+        return "INVALID";
     }
-    return sum;
+}
+
+
+int sumdigit(long card, int len)
+{
+int sum = 0;
+long card_odd = card;
+long card_ev = card;
+if (len % 2 == 0)
+{
+    for (int i = 1; i < len; i += 2)
+    {
+        sum += (card_odd % 10);
+        card_odd /= 100;
+    for (int j = 1; j < len; j += 2)
+    {
+        int digits = (2 * ((card_ev % 100) / 10));
+        if (digits >= 10)
+        {
+            digits =((digits % 10) + (digits / 10));
+        }
+        sum += digits;
+        card_ev /= 100;
+    }
+    }
+}
+else
+{
+    for (int a = 1; a <= len; a += 2)
+    {
+        sum += (card_odd % 10);
+        card_odd /= 100;
+    }
+    for (int b = 2; b < len; b += 2)
+    {
+        int digits = (2 * ((card_ev % 100) / 10));
+        if (digits >= 10)
+        {
+            digits =((digits % 10) + (digits / 10));
+        }
+        sum += digits;
+        card_ev /= 100;
+    }
+}
+return sum;
 }
