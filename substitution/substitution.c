b47c1d5fc845016627b933848f7e56d3c937af62
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-01-21 17:33:02 +0000

    Fri, Jan 21, 2022, 5:33 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -4,9 +4,10 @@
 
 int main(int argc, string argv[])
 {
-    if !(strlen(argv[1]) == 26)
+    if (strlen(argv[1]) != 26)
     {
         printf("Key entered must be exactly 26 characters");
+        printf("\n");
         return 1;
     }
 }
