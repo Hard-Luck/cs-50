94d38227179b3d22ea42dc20a0037e0ab3f58124
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-01-27 17:07:16 +0000

    Thu, Jan 27, 2022, 5:07 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -10,6 +10,7 @@ int main(void)
     string text = get_string("Text: ");
     printf("Letter count is %i\n", count_letters(text));
     printf("Word count is %i\n", count_words(text));
+    printf("Sentence count is %i\n", count_sentence(text));
     return 0;
 }
 
@@ -25,16 +26,28 @@ int count_letters(string text)
     }
     return letter_count;
 }
+int count_word(string text)
+{
+    int letter_count = 0;
+    for (int i = 0; text[i]; i++)
+    {
+        if (text[i] == 32)
+        {
+            letter_count++;
+        }
+    }
+    return lword_count;
+}
 
-int count_words(string text)
+int count_sentence(string text)
 {
     int word_count = 0;
     for (int i = 0; text[i]; i++)
     {
-        if (text[i] == "." || text[i] == "!" || text[i] == "?" )
+        if (text[i] == 46 || text[i] == 21 || text[i] == 63 )
         {
             word_count++;
         }
     }
-    return word_count;
+    return sentence_count;
 }
