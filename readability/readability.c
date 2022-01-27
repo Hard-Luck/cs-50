e67fe3c702782845e89695fbd6d3c09956302044
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-01-27 17:04:08 +0000

    Thu, Jan 27, 2022, 5:04 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,18 +1,40 @@
 #include <stdio.h>
-#include <cs500.h>
+#include <cs50.h>
+#include <ctype.h>
 
-string text = get_string("Text: ")
+int count_words(string text);
+int count_letters(string text);
 
 int main(void)
 {
-
+    string text = get_string("Text: ");
+    printf("Letter count is %i\n", count_letters(text));
+    printf("Word count is %i\n", count_words(text));
+    return 0;
 }
 
 int count_letters(string text)
 {
-    int count = 0;
-    for (int i; string[i], i++)
+    int letter_count = 0;
+    for (int i = 0; text[i]; i++)
     {
-        if 
+        if (isalpha(text[i]))
+        {
+            letter_count++;
+        }
     }
+    return letter_count;
+}
+
+int count_words(string text)
+{
+    int word_count = 0;
+    for (int i = 0; text[i]; i++)
+    {
+        if (text[i] == "." || text[i] == "!" || text[i] == "?" )
+        {
+            word_count++;
+        }
+    }
+    return word_count;
 }
