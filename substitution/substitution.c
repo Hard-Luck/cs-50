18b0cb2270d3d419508a0d0175bd9392743b7059
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-01-27 15:36:32 +0000

    Thu, Jan 27, 2022, 3:36 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -67,7 +67,7 @@ int encrypt_string(string stri, string key)
 {
     for (int j = 0; j < 26; j++)
     {
-        for (int i = 0; stri[i]; i++ )
+        for (int i = 0, n = lenstr(stri); i < n; i++ )
         {
             if ((stri[i]) == 65 + j)
             {
