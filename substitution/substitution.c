1afb8a5f4497304548382a2135511c044f912287
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-01-27 15:31:29 +0000

    Thu, Jan 27, 2022, 3:31 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -75,7 +75,7 @@ int encrypt_string(string stri, string key)
             }
             else if ((stri[i]) == 97 +j)
             {
-                stri[i] = key[j] + 32;
+                stri[i] = toupper(key[j]);
             }
         }
     }
