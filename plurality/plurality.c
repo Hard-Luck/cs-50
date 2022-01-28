289c3c508843548040a3c4a53f2ed0046bf57356
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-01-28 23:16:18 +0000

    Fri, Jan 28, 2022, 11:16 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -81,6 +81,7 @@ bool vote(string name)
 void print_winner(void)
 {
     for (int j = 0; j < candidate_count - 1; i++)
+    {
         for (int i = 0; i < candidate_count - 1; i++)
         {
             if (candidates[i].votes < candidates[i + 1].votes)
@@ -90,6 +91,7 @@ void print_winner(void)
                 candidates[i + 1].votes = tv;
             }
         }
+    }
 
     return;
 }
