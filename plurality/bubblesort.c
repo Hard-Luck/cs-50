50f37d950129a97a472d59ba4b1422a0df5ab161
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-01-29 12:49:31 +0000

    Sat, Jan 29, 2022, 12:49 PM +00:00

--- plurality/bubblesort.c
+++ plurality/bubblesort.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
-int array[] = {1,4,6,5,3};
+int array[5] = {1,4,6,5,3};
 
 int main(void)
 {
-    for (int i = 0, n = ; i < n - 1; i++)
+    for (int i = 0; i < 4; i++)
     {
         if(array[i] < array[i + 1])
         {
-            temp = array[i];
+            int temp = array[i];
             array[i] = array[i -1];
             array[i - 1] = array[i];
         }
