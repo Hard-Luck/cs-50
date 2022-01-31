ca970ab2dfde12f842644f4d202eb257c4bebf1d
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-01-31 18:16:11 +0000

    Mon, Jan 31, 2022, 6:16 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -200,5 +200,12 @@ void lock_pairs(void)
 // Print the winner of the election
 void print_winner(void)
 {
+    for (int i = 0; i < candidate_count ; i++)
+    {
+            for (int j = 0; j < candidate_count ; i++)
+            {
+                int input = 0;
+                if(locked[j][i])
+            }
     return;
 }
