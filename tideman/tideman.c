d2279d6172b48b438b9310276e68784ee0e9d18c
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-01-31 23:09:41 +0000

    Mon, Jan 31, 2022, 11:09 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -132,9 +132,8 @@ void add_pairs(void)
     {
         for(int j = 0 ; j < candidate_count; j++)
         {
-            {
-                if (preferences[i][j] > preferences[j][i])
-                {
+            if (preferences[i][j] > preferences[j][i])
+               {
                     pairs[pair_count].winner = i;
                     pairs[pair_count].loser = j;
                     pair_count++;
