7ab06893f87ed72e6c16bffa9cdfc8286bae7fc8
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-02-01 16:41:50 +0000

    Tue, Feb 1, 2022, 4:41 PM +00:00

--- tideman/test.c
+++ tideman/test.c
@@ -1,10 +1,10 @@
-int test(int n)
+bool test(int n, int m)
 {
     for(int i = 0, i < candidate_count; i++)
     {
         if preference[n][i] = true
         {
-            return test[i]
+            return test(n, m)
         }
     }
 }
