26d1d131dcbcf199fe9f27d3578a4a30bc37b4ba
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-02-01 17:04:02 +0000

    Tue, Feb 1, 2022, 5:04 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -178,7 +178,7 @@ void lock_pairs(void)
 {
     for (int i = 0; i < pair_count; i++)
     {
-        if(test(pairs[i].winner,pairs[i].loser) == 0)
+        if(test(pairs[i].winner,pairs[i].loser))
         {
             locked[pairs[i].winner][pairs[i].loser] = true;
         }
