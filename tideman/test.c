c46949f311a80868675e40b60f032de8e7f4418a
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-02-01 16:17:57 +0000

    Tue, Feb 1, 2022, 4:17 PM +00:00

--- tideman/test.c
+++ tideman/test.c
@@ -1,17 +1,1 @@
-#include <stdio.h>
-
-v = [0,1,2,3,4,5]
-
-for i in v - 1
-    for j=1++ in v
-
-    preferences[i][j]++
-
-
-                pair temp;
-                temp.loser = pairs[j].loser;
-                temp.winner = pairs[j].winner;
-                pairs[j].winner = pairs[j + 1].winner;
-                pairs[j].loser = pairs[j + 1].loser;
-                pairs[j + 1].winner = temp.winner;
-                pairs[j + 1].loser = temp.loser;
+int test()
