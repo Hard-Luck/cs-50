58b7cb66521d2ef7c6c6bb78c2d372509952a587
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-02-01 13:51:11 +0000

    Tue, Feb 1, 2022, 1:51 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -152,17 +152,17 @@ void add_pairs(void)
 // Sort pairs in decreasing order by strength of victory
 void sort_pairs(void)
 {
-    for (int i = pair_count - 1; i >= 0; i--)
+    for (int i = 0; i < pair_count; i++)
     {
-        for (int j = pair_count; j > 0 + i; j--)
+        for (int j = pair_count - 1; j >= 0 + i; j--)
         {
-            int margin = pairs[j + 1].winner - pairs[j + 1].loser;
+            int margin = pairs[j - 1].winner - pairs[j - 1].loser;
             int next_margin = pairs[j].winner - pairs[j].loser;
-            if (margin > next_margin)
+            if (margin < next_margin)
             {
                 pair temp = pairs[j];
-                pairs[j] = pairs[j + 1];
-                pairs[j + 1] = temp;
+                pairs[j] = pairs[j - 1];
+                pairs[j - 1] = temp;
             }
         }
     }
