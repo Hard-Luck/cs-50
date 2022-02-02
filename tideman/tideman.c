31fcc33ac92c64ea740346ac36b828ecb982fb58
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-02-02 16:21:41 +0000

    Wed, Feb 2, 2022, 4:21 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -212,18 +212,18 @@ void print_winner(void)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        int counter = 0;
+        bool counter = true;
         for (int j = 0; j < candidate_count; j++)
         {
             if (j != i)
             {
                 if(preferences[j][i])
                 {
-                    counter++;
+                    counter = false;
                 }
             }
         }
-        if (counter == 0)
+        if (counter == true)
         {
             printf ("%s",candidates[i]);
         }
