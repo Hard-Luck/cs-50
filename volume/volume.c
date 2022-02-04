7e40e8e61db23480ff07e7500d123345dc49d2ee
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-02-04 15:07:40 +0000

    Fri, Feb 4, 2022, 3:07 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -33,7 +33,9 @@ int main(int argc, char *argv[])
 
     float factor = atof(argv[3]);
 
-    // TODO: Copy header from input file to output file
+    //Copy header from input file to output file
+    header_arr[HEADER_SIZE]
+    fwrite()
 
     // TODO: Read samples from input file and write updated data to output file
 
