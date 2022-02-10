186798fdd9e42698e8e679f0c306e37b23a409ec
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-02-10 13:46:25 +0000

    Thu, Feb 10, 2022, 1:46 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -35,8 +35,8 @@ int main(int argc, char *argv[])
 
     //Copy header from input file to output file
     uint8_t header_arr[HEADER_SIZE]
-    fread(&header_arr,1,HEADER_SIZE, input);
-    fwrite(&header_arr,1,HEADER_SIZE, output);
+    fread(&header_arr,sizeof(uint8_t),HEADER_SIZE, input);
+    fwrite(&header_arr,sizeof(uint8_t),HEADER_SIZE, output);
     // TODO: Read samples from input file and write updated data to output file
 
     // Close files
