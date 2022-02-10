f4b214dae270a38db47a52b44653fe7678573992
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-02-10 14:31:48 +0000

    Thu, Feb 10, 2022, 2:31 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -35,18 +35,18 @@ int main(int argc, char *argv[])
 
     //Copy header from input file to output file
     uint8_t header[HEADER_SIZE];
-    fread(header,sizeof(uint8_t), 1, input);
-    fwrite(header,sizeof(uint8_t), 1, output);
+    fread(header,HEADER_SIZE, 1, input);
+    fwrite(header,HEADER_SIZE, 1, output);
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer[10000];
-    size_t num_read = fread(&buffer, sizeof(int16_t), 1, input);
+    size_t num_read = fread(&buffer, siz, 1, input);
     buffer[num_read] = '\0';
     int16_t double_buffer[num_read];
     for (int i = 0; i <= num_read; i++)
     {
         double_buffer[i] = factor * buffer[i];
     }
-    fwrite(&double_buffer, sizeof(int16_t), 1, output);
+    fwrite(&double_buffer, num_read, 1, output);
     // Close files
     fclose(input);
     fclose(output);
