d1b99ea4d5156202b5cc37d98eb698ff86b95903
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-02-10 21:26:22 +0000

    Thu, Feb 10, 2022, 9:26 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -3,11 +3,17 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    for (int i = 0; i < height, i++)
+    for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            int red = image[i][j]
+            int blue = image[i][j].rgbtBlue;
+            int green = image[i][j].rgbtGreen;
+            int red = image[i][j].rgbtRed;
+            int average = (blue + green + red)/3;
+            image[i][j].rgbtBlue = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtRed = average;
         }
     }
     return;
