cd92bcf8c21a41dd402b845001749d089b011d44
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-02-10 21:41:33 +0000

    Thu, Feb 10, 2022, 9:41 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -26,7 +26,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width/2; j++)
         {
-            int temp = image[i][j];
+            RGBTRIPLE temp = image[i][j];
             image[i][j] = image[i][width - j];
             image[i][width - j] = temp;
         }
