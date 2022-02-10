5cfdb12b243d4484a1c1408824bc59e847fae563
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-02-10 21:39:23 +0000

    Thu, Feb 10, 2022, 9:39 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -22,6 +22,15 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width/2; j++)
+        {
+            int temp = image[i][j];
+            image[i][j] = image[i][j - 1 -];
+            image[i][j] = average;
+        }
+    }
     return;
 }
 
