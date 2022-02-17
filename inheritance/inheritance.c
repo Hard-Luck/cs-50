63fd343de49150bc9a21ab1d02f847477eafe250
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-02-17 22:49:29 +0000

    Thu, Feb 17, 2022, 10:49 PM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -49,7 +49,8 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        
+        child.parent[0] = parent0;
+        child.parent[1] = parent1;
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
 
     }
