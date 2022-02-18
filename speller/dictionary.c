57b2951bda08220510d7db6af8cfd364cbcdc932
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-02-18 15:57:23 +0000

    Fri, Feb 18, 2022, 3:57 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -32,6 +32,8 @@ unsigned int hash(const char *word)
     return toupper(word[0]) - 'A';
 }
 
+
+
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
