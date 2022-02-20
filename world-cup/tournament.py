c569c4b4660f29d1090dae1a691300bcda2a8584
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-02-20 18:40:46 +0000

    Sun, Feb 20, 2022, 6:40 PM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -22,7 +22,8 @@ def main():
             teams += [{"name" : row["Name"],"rating" : int(row["Rating"])}]
 
     counts = {}
-    # TODO: Simulate N tournaments and keep track of win counts
+    # Simulate N tournaments and keep track of win counts
+    
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
