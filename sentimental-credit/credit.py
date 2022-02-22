9eb91f84739c1e48fb4547fc4e1685f594f4e28f
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-02-22 13:43:06 +0000

    Tue, Feb 22, 2022, 1:43 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,7 +1,9 @@
 import cs50
 import re
 
-pattern = r"/^[0-9]{13}$/"
+pattern = r"[0-9]{13}|[0-9]{15}|[0-9]{16}"
 
-if re.match(pattern,"1111111111111"):
+if re.match(pattern,"111111111111111111111"):
     print("all good")
+else:
+    print("baD")
