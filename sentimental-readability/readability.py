3c246f7fe876eb5e18e22646519301d9aa8b2810
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-02-22 17:19:19 +0000

    Tue, Feb 22, 2022, 5:19 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -5,5 +5,5 @@ text = cs50.get_string("Enter Text: ")
 
 words = len(text.split())
 
-nopunc = text.translate(str.maketrans("","",string.punctuation()))
-print(nopunc)
+nopunc = text.translate(str.maketrans("","",string.punctuation))
+print(nopunc.strip())
