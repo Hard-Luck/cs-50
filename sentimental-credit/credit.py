e91899ba85c35cf72aba27989d08ed6f967b654c
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-02-22 13:47:17 +0000

    Tue, Feb 22, 2022, 1:47 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,9 +1,9 @@
 import cs50
 import re
 
-pattern = r"/[0-9]{7}/"
+pattern = r"/^[0-9]{7}$/"
 
-if re.match(pattern,"1111111"):
+if re.match(pattern,"0000000"):
     print("all good")
 else:
     print("baD")
