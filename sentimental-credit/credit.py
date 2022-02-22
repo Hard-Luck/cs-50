cd8463b4c2c8ba2b086daf47780f9352abddfbd8
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-02-22 15:45:07 +0000

    Tue, Feb 22, 2022, 3:45 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -25,22 +25,7 @@ def check_length(number):
 # Sum check function to check card number is valid
 def sum_check(number):
     sum = 0
-    length = len(number)
-    hlen = length // 2
-    if len(number) % 2 == 1:
-        for i in range(hlen + 1):
-            sum += int(number[2 * i])
-        for j in range(1, hlen + 1):
-            sum += 2*(int(number[(2 * j) - 1])) % 10
-            if int(number[2*j]) >= 5:
-                sum += 1
 
-    else:
-        for k in range(hlen):
-            sum += (2 * (int(number[2 * k])) % 10)
-            if int(number[2*k]) >= 5:
-                sum += 1
-            sum += (int(number[(2 * k + 1)]))
 
 
     print(sum)
