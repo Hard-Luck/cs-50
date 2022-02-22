0b13b254a0297071a1c48471c7691c77298dda37
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-02-22 13:11:38 +0000

    Tue, Feb 22, 2022, 1:11 PM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,6 +1,9 @@
 import cs50
 
-height = cs50.get_int("How tall do you want it?: ")
+while true:
+    height = cs50.get_int("How tall do you want it?: ")
+    if height > 0:
+        break
 
 for i in range(height):
     print((height - 1 - i) * " " + i * "#" + "# #" + i * "#")
