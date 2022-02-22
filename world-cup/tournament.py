eea1af099f1e3c96f5f88f7680d23974fcb9523c
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-02-22 12:34:22 +0000

    Tue, Feb 22, 2022, 12:34 PM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -24,7 +24,11 @@ def main():
     # Simulate N tournaments and keep track of win counts
     for i in range(N):
         winner = simulate_tournament(teams)
-        counts[winner["name"]] += 1
+        if not counts[winner["name"]]:
+            counts[winner[name]] = 1
+        else:
+            counts[winner[name]] += 1
+
 
 
     # Print each team's chances of winning, according to simulation
