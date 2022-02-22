eed00019fc22795d294af41a3412d4dc8ffd8c8b
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-02-22 13:46:09 +0000

    Tue, Feb 22, 2022, 1:46 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,9 +1,9 @@
 import cs50
 import re
 
-pattern = r"[0-9]{13}|[0-9]{15}|[0-9]{16}"
+pattern = r"/[0-9]{7}/"
 
-if re.match(pattern,"111111111111111111111"):
+if re.match(pattern,"1111111"):
     print("all good")
 else:
     print("baD")
