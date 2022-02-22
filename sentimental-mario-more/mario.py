dd6209ccb5615610ea8c1a48a9e2c52626611d81
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-02-22 13:17:55 +0000

    Tue, Feb 22, 2022, 1:17 PM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,11 +1,12 @@
 import cs50
 
 height = 0
-
+# Loop, continuing to ask for a heigh if not between 1 - 8
 while True:
     height = cs50.get_int("How tall do you want it?: ")
     if 9 > height > 0:
         break
 
+# Print the blocks
 for i in range(height):
     print((height - 1 - i) * " " + i * "#" + "#  #" + i * "#")
