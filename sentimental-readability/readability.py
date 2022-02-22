32316eb37cc1f603fbac9c60e3f44d00b8ac98be
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-02-22 17:23:24 +0000

    Tue, Feb 22, 2022, 5:23 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -2,10 +2,17 @@ import cs50
 import string
 
 text = cs50.get_string("Enter Text: ")
+
+# Take all punctuation out of the text 
 nopunc = text.translate(str.maketrans("","",string.punctuation))
 
+# Split the text at every space, count the size of the list
 words = len(text.split())
 letters = len(nopunc)
 sentences = 0
 
+sentences += text.count(".")
+sentences += text.count("!")
+sentences += text.count("?")
 
+print(sentences)
