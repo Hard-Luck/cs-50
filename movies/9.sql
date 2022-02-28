98a2baceb3becb28a96494556c0b5832f47de40e
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-02-28 22:05:54 +0000

    Mon, Feb 28, 2022, 10:05 PM +00:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT name from people WHERE id in
-(SELECT person_id from stars where movie_id IN
+SELECT name from people WHERE id in
+(SELECT DISTINCT person_id from stars where movie_id IN
 (SELECT id FROM movies WHERE year = 2004))
 ORDER BY birth;
