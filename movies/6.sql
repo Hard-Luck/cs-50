ec1635ad39462f019659e03e2d01d001ab4950d2
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-02-28 21:28:37 +0000

    Mon, Feb 28, 2022, 9:28 PM +00:00

--- movies/6.sql
+++ movies/6.sql
@@ -1,1 +1,1 @@
-SELECT avg(rating) FROM ratings WHERE id IN (SELECT year FROM movies WHERE year = 2012);
+SELECT avg(rating) FROM ratings WHERE movie_id IN (SELECT id FROM movies WHERE year = 2012);
