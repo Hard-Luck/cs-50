93bdad8f9a1537df761202d7235c18b5a6d5e302
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-02-28 22:14:30 +0000

    Mon, Feb 28, 2022, 10:14 PM +00:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,3 +1,3 @@
-SELECT person_id FROM stars WHERE person_id in(
+SELECT name FROM people WHERE id in(
 SELECT DISTINCT person_id from directors where movie_id IN(
 SELECT movie_id from ratings WHERE rating >= 9));
