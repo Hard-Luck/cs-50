59cb6639be926b73727b29ddd32006f4fb23afce
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-02-28 22:21:39 +0000

    Mon, Feb 28, 2022, 10:21 PM +00:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,5 +1,5 @@
 SELECT title FROM movies WHERE id in(
 SELECT movie_id from stars where person_id IN(
 SELECT id from people WHERE name LIKE "Chadwick Boseman"))
-ORDER BY rating
+ORDER BY ratings
 LIMIT 5;
