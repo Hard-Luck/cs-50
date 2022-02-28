b0ae4ce9e3643462e5257066170867f573005ebc
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-02-28 21:58:04 +0000

    Mon, Feb 28, 2022, 9:58 PM +00:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,2 +1,3 @@
+SELECT title from movies where id in (
 SELECT person_id from stars where movie_id IN
-(SELECT id FROM movies WHERE title LIKE "toy story");
+(SELECT id FROM movies WHERE title LIKE "toy story"));
