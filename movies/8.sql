b152afdd779bb1cc724e46c40356f6ef7f19a18f
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-02-28 22:02:40 +0000

    Mon, Feb 28, 2022, 10:02 PM +00:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,3 +1,4 @@
-SELECT name from people WHERE id in
-(SELECT person_id from stars where movie_id IN
+SELECT name from people where id in
+(
+SELECT person_id from stars where movie_id IN
 (SELECT id FROM movies WHERE title LIKE "toy story"));
---
+++ movies/9.sql
@@ -0,0 +1,3 @@
+SELECT DISTINCT name from people WHERE id in
+(SELECT person_id from stars where movie_id IN
+(SELECT id FROM movies WHERE year = 2004));
