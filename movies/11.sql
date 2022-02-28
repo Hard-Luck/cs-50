9d04743a6a7b4f69e16c1a17025fa7a4f3f0a52a
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-02-28 22:35:42 +0000

    Mon, Feb 28, 2022, 10:35 PM +00:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,6 +1,9 @@
-SELECT title from movies where id in(
+SELECT movies.title, ratings.rating
+FROM movies
+INNER JOIN ratings ON movies.id = ratings.movie_id
+WHERE id IN(
 SELECT movie_id from ratings where movie_id IN(
 SELECT movie_id from stars where person_id IN(
-SELECT id from people WHERE name LIKE "Chadwick Boseman"))
-ORDER BY rating)
+SELECT id from people WHERE name LIKE "Chadwick Boseman")))
+ORDER BY rating DESC
 LIMIT 5;
