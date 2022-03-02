747c15cecd329808beb7264e3da684e4fbd2e6a3
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-03-02 13:01:32 +0000

    Wed, Mar 2, 2022, 1:01 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,6 +1,7 @@
-SELECT * FROM movies
+SELECT DISTINCT person_id from stars where movie_id in(
+SELECT movie_id FROM movies
 JOIN stars on movies.id = stars.movie_id
 JOIN people ON stars.person_id = people.id
-where name LIKE "Kevin Bacon" and birth = 1958;
+where name LIKE "Kevin Bacon" and birth = 1958);
 
 
