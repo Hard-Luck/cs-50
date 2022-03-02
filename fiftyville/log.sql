4e44b0229b1077e35a97994fc6aad179cd67d468
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-03-02 15:10:57 +0000

    Wed, Mar 2, 2022, 3:10 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,3 +1,4 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
--- Check description from scrime scene reports
-SELECT description FROM crime_scene_reports WHERE time = ;
+-- Check description from scrime scene reports for 28th July
+SELECT description FROM crime_scene_reports WHERE (day = 28 and month = 7) ;
+--10:15 am all 3 witnesses mention the
