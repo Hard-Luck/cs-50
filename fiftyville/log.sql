d3759ca02cbaacd695943f967106b60426734182
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-03-02 15:42:46 +0000

    Wed, Mar 2, 2022, 3:42 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -17,10 +17,3 @@ LIMIT 1);
 --Name: Venessa
 
 --Check Venessas ID agianst flight records
-SELECT * from flights
-join people on flights.id = people.id
-where people.id in(
-SELECT id from people where license_plate in(
-SELECT license_plate FROM bakery_security_logs
-WHERE (day = 28 and month = 7 and hour = 10 and minute > 15)
-LIMIT 5));
