23db2526265e42e28e7058186dad406045fc9b42
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-03-02 13:04:01 +0000

    Wed, Mar 2, 2022, 1:04 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -4,6 +4,7 @@ WHERE movie_id in(
 SELECT movie_id FROM movies
 JOIN stars on movies.id = stars.movie_id
 JOIN people ON stars.person_id = people.id
-where name LIKE "Kevin Bacon" and birth = 1958);
+where name LIKE "Kevin Bacon" and birth = 1958)
+ORDER BY name;
 
 
