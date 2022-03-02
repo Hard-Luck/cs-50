d851e6f0fe338cc9964705cc4c1d40fe28b1e4c9
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-03-02 15:19:38 +0000

    Wed, Mar 2, 2022, 3:19 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -6,4 +6,5 @@ SELECT description FROM crime_scene_reports
 
 --Check bakery security logs for 10:15 on 28th July
 SELECT * FROM bakery_security_logs
-WHERE (day = 28 and month = 7 and hour = 10);
+WHERE (day = 28 and month = 7 and hour = 10 and minute > 15)
+LIMIT 1;
