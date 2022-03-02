263287c638eb01b65fdcf5235039a6a5b5de0007
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-03-02 15:50:04 +0000

    Wed, Mar 2, 2022, 3:50 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -23,9 +23,13 @@ SELECT license_plate FROM bakery_security_logs
 WHERE (day = 28 and month = 7 and hour = 10 and minute > 15)
 LIMIT 1));
 --Vanessa took 3 flights, check flight ID against date
+
 SELECT * from flights where id in(
 SELECT flight_id from passengers where passport_number in(
 SELECT passport_number from people where license_plate in(
 SELECT license_plate FROM bakery_security_logs
 WHERE (day = 28 and month = 7 and hour = 10 and minute > 15)
-LIMIT 1)));
+LIMIT 1)) and day = 28 and month = 7);
+
+--destination airport 8
+select * from airports where id = 8;
