0a1d85714efa27964bb016111e43e10943eef3da
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-03-02 12:54:27 +0000

    Wed, Mar 2, 2022, 12:54 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,4 +1,11 @@
 SELECT title FROM movies
-JOIN stars on movies.id = stars.person_id
+JOIN stars on movies.id = stars.movie_id
 JOIN people ON stars.person_id = people.id
-where name LIKE "Johnny Depp";
+where name LIKE "Johnny Depp"
+
+INTERSECT
+
+SELECT title FROM movies
+JOIN stars on movies.id = stars.movie_id
+JOIN people ON stars.person_id = people.id
+where name LIKE "Helena Bonham Carter"
