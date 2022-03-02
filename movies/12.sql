e7a297c5d0b8c77fa4316dccad47fb34721becec
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-03-02 12:00:10 +0000

    Wed, Mar 2, 2022, 12:00 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,5 +1,3 @@
 select title from movies where id IN(
-SELECT movie_id from stars WHERE person_id IN ((
-SELECT id from people WHERE name LIKE "Johnny Depp")
-AND(
-SELECT id from people WHERE name LIKE "Helena Bonham Carter")));
+SELECT movie_id from stars WHERE person_id IN (
+SELECT id from people WHERE name LIKE "Johnny Depp"))
