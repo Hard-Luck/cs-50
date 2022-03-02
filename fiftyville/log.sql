94b2392807073436cc1fc40e4d23428289c7e06e
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-03-02 15:16:03 +0000

    Wed, Mar 2, 2022, 3:16 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,4 +1,9 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 -- Check description from scrime scene reports for 28th July
-SELECT description FROM crime_scene_reports WHERE (day = 28 and month = 7) ;
+SELECT description FROM crime_scene_reports
+       WHERE (day = 28 and month = 7) ;
 --10:15 am all 3 witnesses mention the
+
+--Check bakery security logs for 10:15 on 28th July
+SELECT * FROM bakery_security_logs
+       WHERE (day = 28 and month = 7) ;
