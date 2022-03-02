008bfd436e460aa9a7fe518e5482956ad983cc04
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-03-02 12:15:05 +0000

    Wed, Mar 2, 2022, 12:15 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,5 +1,5 @@
 SELECT movie_id from stars WHERE person_id IN (
 SELECT id from people WHERE name LIKE "Johnny Depp" AND
-SELECT id from people WHERE name LIKE "HELENA BONHAM CARTER");
+"HELENA BONHAM CARTER");
 
 --select title from movies where id IN(
