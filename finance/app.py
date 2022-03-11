b0b2d87a345371e1802c1a1e6c7d4d2df55f21c4
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-03-11 12:15:54 +0000

    Fri, Mar 11, 2022, 12:15 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -43,7 +43,8 @@ def after_request(response):
 @login_required
 def index():
     """Show portfolio of stocks"""
-    holdings = db.execute("SELECT stock, quantity FROM stocks where person_id = ?", session["user_id"])
+    holdings = db.execute(
+        "SELECT stock, quantity FROM stocks where person_id = ?", session["user_id"])
     prices = {}
     total = 0
     # Loop through holdings for tickers and price calculations
@@ -56,7 +57,8 @@ def index():
 @app.route("/buy", methods=["GET", "POST"])
 @login_required
 def buy():
-    balance = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])
+    balance = db.execute(
+        "SELECT cash FROM users WHERE id = ?", session["user_id"])
     """Buy shares of stock"""
     if request.method == "POST":
         stock = lookup(request.form.get("quote"))
@@ -65,23 +67,29 @@ def buy():
             quantity = float(request.form.get("quantity"))
             cost = price * quantity
             print(balance)
-            #check balance
+            # check balance
             if not cost < float(balance[0]["cash"]):
                 return apology("Not enough funds")
             new_balance = float(balance[0]["cash"]) - cost
-            stock_check = db.execute("SELECT stock FROM stocks WHERE stock = ? AND person_id =?", stock["symbol"].upper(), session["user_id"])
-            db.execute("UPDATE users SET cash = ? where id = ?", new_balance, session["user_id"])
-            #check if user already owns some of this stock, if not insert new row
+            stock_check = db.execute(
+                "SELECT stock FROM stocks WHERE stock = ? AND person_id =?", stock["symbol"].upper(), session["user_id"])
+            db.execute("UPDATE users SET cash = ? where id = ?",
+                       new_balance, session["user_id"])
+            # check if user already owns some of this stock, if not insert new row
             if not stock_check:
-                db.execute("INSERT INTO stocks (stock, person_id, quantity) VALUES(?, ?, ?)",stock["symbol"].upper(), session["user_id"], int(quantity))
+                db.execute("INSERT INTO stocks (stock, person_id, quantity) VALUES(?, ?, ?)",
+                           stock["symbol"].upper(), session["user_id"], int(quantity))
                 return render_template("buy.html", cash=new_balance)
             # Query how many stock already owned
-            current = db.execute("SELECT quantity FROM stocks WHERE stock = ? AND person_id =?", stock["symbol"].upper(), session["user_id"])
-            db.execute("UPDATE stocks set quantity = ? WHERE person_id = ?", int(quantity) + current[0]["quantity"], session["user_id"])
+            current = db.execute("SELECT quantity FROM stocks WHERE stock = ? AND person_id =?",
+                                 stock["symbol"].upper(), session["user_id"])
+            db.execute("UPDATE stocks set quantity = ? WHERE person_id = ?", int(
+                quantity) + current[0]["quantity"], session["user_id"])
             db.execute(
-                        "INSERT INTO history (date, buysell, person_id, stock, quantity) VALUES(DATETIME(), ?, ?, ?,?)",
-                        "BUY", session["user_id"],stock["symbol"].upper(), int(quantity), price
-                        )
+                "INSERT INTO history (date, buysell, person_id, stock, quantity) VALUES(DATETIME(), ?, ?, ?,?)",
+                "BUY", session["user_id"], stock["symbol"].upper(), int(
+                    quantity), price
+            )
             return render_template("buy.html", cash=new_balance)
         else:
             return apology("Stock does not exist")
@@ -115,7 +123,8 @@ def login():
             return apology("must provide password", 403)
 
         # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        rows = db.execute("SELECT * FROM users WHERE username = ?",
+                          request.form.get("username"))
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
@@ -157,7 +166,6 @@ def quote():
         return render_template("quote.html")
 
 
-
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -165,14 +173,17 @@ def register():
         name = request.form.get("name").rstrip()
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
-        password = generate_password_hash(password1, method='pbkdf2:sha256', salt_length=8)
+        password = generate_password_hash(
+            password1, method='pbkdf2:sha256', salt_length=8)
         if not name or not password2 or not (password1 == password2):
             return apology("Form not entered correctly")
-        #is name in database
-        name_check = db.execute("SELECT username FROM users where UPPER(username) = ?",name.upper())
+        # is name in database
+        name_check = db.execute(
+            "SELECT username FROM users where UPPER(username) = ?", name.upper())
         print(name_check)
         if not name_check:
-            db.execute("INSERT INTO users (username, hash) VALUES (?,?)", name, password)
+            db.execute(
+                "INSERT INTO users (username, hash) VALUES (?,?)", name, password)
         else:
             return apology("Username already exists")
         return render_template("login.html")
@@ -184,8 +195,10 @@ def register():
 @login_required
 def sell():
     """Sell shares of stock"""
-    balance = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])
-    stocks = db.execute("SELECT stock FROM stocks where person_id = ? AND quantity > 0",  session["user_id"])
+    balance = db.execute(
+        "SELECT cash FROM users WHERE id = ?", session["user_id"])
+    stocks = db.execute(
+        "SELECT stock FROM stocks where person_id = ? AND quantity > 0",  session["user_id"])
 
     if request.method == "POST":
         to_sell = request.form.get("sell")
@@ -193,10 +206,11 @@ def sell():
             return apology("Sale Error")
         try:
             quantity = float(request.form.get("quantity"))
-            quantity_owned = db.execute("SELECT quantity FROM stocks WHERE stock = ? AND person_id =?", to_sell, session["user_id"])
+            quantity_owned = db.execute(
+                "SELECT quantity FROM stocks WHERE stock = ? AND person_id =?", to_sell, session["user_id"])
             owned = quantity_owned[0]["quantity"]
         except:
-               return apology("Sale Error")
+            return apology("Sale Error")
 
         # Ensure quantity is positive and less tthan or equal to stock owned
         if (quantity > owned) or quantity < 0:
@@ -209,16 +223,17 @@ def sell():
         print(balance)
         # Update cash balance and stock balance
         new_balance = float(balance[0]["cash"]) + sale_price
-        db.execute("UPDATE users SET cash = ? where id = ?", new_balance, session["user_id"])
-        db.execute("UPDATE stocks SET quantity = ? where person_id = ? and stock = ?", new_owned, session["user_id"], to_sell)
+        db.execute("UPDATE users SET cash = ? where id = ?",
+                   new_balance, session["user_id"])
+        db.execute("UPDATE stocks SET quantity = ? where person_id = ? and stock = ?",
+                   new_owned, session["user_id"], to_sell)
         # If user sells all stock, delete from db
-        stocks = db.execute("SELECT stock FROM stocks where person_id = ? AND quantity > 0",  session["user_id"])
+        stocks = db.execute(
+            "SELECT stock FROM stocks where person_id = ? AND quantity > 0",  session["user_id"])
         db.execute(
-                    "INSERT INTO history (date, buysell, person_id, stock, quantity, price) VALUES(DATETIME(), ?, ?, ?,?)",
-                    "SELL", session["user_id"],to_sell, int(quantity), price
-                  )
+            "INSERT INTO history (date, buysell, person_id, stock, quantity, price) VALUES(DATETIME(), ?, ?, ?,?)",
+            "SELL", session["user_id"], to_sell, int(quantity), price
+        )
         return render_template("sell.html", cash=new_balance, stocks=stocks)
 
     return render_template("sell.html", cash=balance[0]["cash"], stocks=stocks)
-
-
