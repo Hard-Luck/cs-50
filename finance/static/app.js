bda553164ff1921c498a77f2e97d840a805a2db1
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-03-13 13:04:07 +0000

    Sun, Mar 13, 2022, 1:04 PM +00:00

--- finance/static/app.js
+++ finance/static/app.js
@@ -2,6 +2,6 @@ let input = document.getElementByID('sell');
             input.addEventListener("input", async function() {
                 let response = await fetch("https://cloud.iexapis.com/stable/stock/${input}/quote?token=pk_68b782ab6d7b4d3caac3671fc5a1c489");
                 let stock = await response.json();
-                let html = "<li>" stock.latest.Price "</li>"
+                let html = "<li>" stock.latestPrice "</li>";
                 document.querySelector('ul').innerHTML = html;
             });
