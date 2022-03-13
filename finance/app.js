b178d17987f7b5f5c103ccc6707f83c65679cdd1
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-03-13 12:44:31 +0000

    Sun, Mar 13, 2022, 12:44 PM +00:00

--- finance/app.js
+++ finance/app.js
@@ -2,7 +2,7 @@
 
 let input = document.querySelector('input');
             input.addEventListener('input', async function() {
-                let response = await fetch("https://cloud.iexapis.com/?filter=symbol,volume,lastSalePrice" + input.value);
+                let response = await fetch("https://cloud.iexapis.com/stable/stock/{}/quote?token=pk_68b782ab6d7b4d3caac3671fc5a1c489" + input.value);
                 let shows = await response.json();
                 let html = '';
                 for (let id in shows) {
