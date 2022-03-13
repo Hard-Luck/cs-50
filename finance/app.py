13ff924a7a771512918d0c014618e4e4b1a78f77
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-03-13 16:12:23 +0000

    Sun, Mar 13, 2022, 4:12 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -94,10 +94,11 @@ def buy():
                 "INSERT INTO history (date, buysell, person_id, stock, quantity, price) VALUES(DATETIME(), ?, ?, ?, ?, ?)",
                 "BUY", session["user_id"], stock["symbol"].upper(), int(quantity), price
             )
-            cost = usd(cost)
-            print(new_balance)
-            message = f"Sale for {cost} completed {new_balance} remaining"
-            return render_template("buy.html", cash=new_balance, message=message)
+            #cost = usd(cost)
+            #print(new_balance)
+            #message = f"Sale for {cost} completed {new_balance} remaining"
+            #return render_template("buy.html", cash=new_balance, message=message)
+            return redirect("/")
 
         else:
             return apology("Stock does not exist")
