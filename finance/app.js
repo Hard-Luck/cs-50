95e49777038493fd25a3907b287e5abfe9d24550
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-03-13 12:33:47 +0000

    Sun, Mar 13, 2022, 12:33 PM +00:00

--- finance/app.js
+++ finance/app.js
@@ -1,1 +1,13 @@
-?filter=symbol,volume,lastSalePrice
+
+
+let input = document.querySelector('input');
+            input.addEventListener('input', async function() {
+                let response = await fetch("https://cloud.iexapis.com/?filter=symbol,volume,lastSalePrice" + input.value);
+                let shows = await response.json();
+                let html = '';
+                for (let id in shows) {
+                    let title = shows[id].title.replace('<', '&lt;').replace('&', '&amp;');
+                    html += '<li>' + title + '</li>';
+                }
+                document.querySelector('ul').innerHTML = html;
+            });
