ac847ac300502f042cb6e3e0dee24cfc1aef3eed
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-03-28 17:31:21 +0000

    Mon, Mar 28, 2022, 5:31 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+        // Open input file
+    FILE *inptr = fopen(infile, "r");
+    if (inptr == NULL)
+    {
+        printf("Could not open %s.\n", infile);
+        return 4;
+    }
 }
