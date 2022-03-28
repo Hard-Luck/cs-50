65ebad6fb91366b6e1a8af75df5a4fb45925d3ba
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-03-28 18:48:49 +0000

    Mon, Mar 28, 2022, 6:48 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -36,8 +36,14 @@ int main(int argc, char *argv[])
             buffer[2] == 0xff &&
             (buffer[3] & 0xf0) == 0xe0)
         {
-
+            if count != 0:
+            {
+                fclose(img_ptr)
+            }
+            sprintf(filename, "%03i.jpg", count);
+            img_pointer = fopen(filename, "w");
+            count++;
         }
-
+        
     }
 }
