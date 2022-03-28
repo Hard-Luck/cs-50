da2ccff9b46fd2fb55c11b545bb28deb061fbbae
GrGadget <72257311+GrGadget@users.noreply.github.com>
2022-03-28 18:02:08 +0000

    Mon, Mar 28, 2022, 6:02 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -3,14 +3,22 @@
 
 int main(int argc, char *argv[])
 {
+
+    // Ensure arv length is 2
+    if (len(argv) != 2)
+    {
+        printf("Error, too many arguments")
+        return 1
+    }
         // Remember filenames
-    char *infile = argv[optind];
-    
+    char *infile = argv[1];
+
         // Open input file
     FILE *inptr = fopen(infile, "r");
     if (inptr == NULL)
     {
         printf("Could not open %s.\n", infile);
-        return 4;
+        return 1;
     }
+    
 }
